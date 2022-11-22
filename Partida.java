fbce1522d2cc197f93f8ab8f8f67492198e33b38
David Julián Largo <115720002+DeJulians@users.noreply.github.com>
2022-11-22 18:02:13 +0100

    Update Partida.java
    get set
    equals

--- Partida.java
+++ Partida.java
@@ -13,31 +13,81 @@ public class Partida {
     //Atributes
     private int numPalabras;
     private Jugador jugador1, jugador2;
-    private int ganadosJug1;
-    private int ganadosJug2;
-    private final static int MAX_PALABRAS = 10; 
-    private PartidaPalabra[] ListaPalabras;  
-    
-    //Constructors
+    private int ganadosJug1, ganadosJug2;
+    private PartidaPalabra[] listaPalabras;
+    private final static int MAXPALABRAS = 10;
+    //Constructores
     public Partida(Jugador j1, Jugador j2) {
         this(j1);
-        if (j2 != null) jugador2=j2;
+        if (j2 != null)
+            jugador2 = j2;
     }    
     
     public Partida(Jugador j) {
-        ListaPalabras = new PartidaPalabra[10]; 
-        if (j != null) jugador1=j;
+        listaPalabras = new PartidaPalabra[MAXPALABRAS];
+        numPalabras = -1; 
+        if (j != null)
+            jugador1 = j;
+    }
+
+    //Get Set
+    public Jugador getJugador1(){
+        return jugador1;
+    }
+
+    public Jugador getJugador2(){
+        return jugador2;
+    }
+
+    public int getNumPalabras(){
+        return numPalabras;
+    }
+
+    public int getGanadosJug1(){
+        return ganadosJug1;
+    }
+
+    public int getGanadosJug2(){
+        return ganadosJug2;
+    }
+
+    public PartidaPalabra [] getListaPalabras(){
+        return listaPalabras;
+    }
+
+    public void setNumPalabras(int n){
+        numPalabras = n;
+    }
+
+    public void setGanadosJug1(int n){
+        ganadosJug1 = n;
+    }
+
+    public void setGanadosJug2(int n){
+        ganadosJug2 = n;
     }
     
-    //Methods
+    //Metodos
     public void crearPartida(Palabra p) {
         crearIndividual(jugador1, p);
         crearIndividual(jugador2, p);
     }
     
     private void crearIndividual(Jugador j, Palabra p) {
-        ListaPalabras[numPalabras++]= new PartidaPalabra(j,p);
+        getListaPalabras()[(getNumPalabras() + 1)] = new PartidaPalabra(j,p);
         
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o)
+            return true;
+        if (o == null)
+            return false;
+        if (getClass() != o.getClass())
+            return false;
+        Partida p = (Partida) o;
+        return ((getJugador1().equals(p.getJugador1())) && (getJugador2().equals(o.getJugador2())) && (getNumPalabras() == p.getNumPalabras()) && (getGanadosJug1() == p.getGanadosJug1()) && (getGanadosJug2() == p.ganadosJug2()) && (getListaPalabras().equals(p.getListaPalabras())));
     }
     
 }
