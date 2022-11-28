5da24cac1ca41d8f4688152dbe017fa120f00480
David Julián Largo <115720002+DeJulians@users.noreply.github.com>
2022-11-28 23:24:56 +0100

    modificar Wordle constructor + un par de metodos

--- src/PracticaWordle/Wordle.java
+++ src/PracticaWordle/Wordle.java
@@ -11,7 +11,44 @@ import java.util.ArrayList;
  */
 public class Wordle {
 
+    //Atributes
     private ArrayList <Partida> listaPartidas;
     private ArrayList <Jugador> listaJugadores;
-    
+
+    //Constructors
+    public Wordle(){
+        listaPartidas = new ArrayList <Partida> ();
+        listaJugadores = new ArrayList <Jugador> ();
+    }
+
+    //Getters
+    public ArrayList <Partida> getListaPartidas(){
+        return listaPartidas;
+    }
+
+    public ArrayList <Jugador> getListaJugadores(){
+        return listaJugadores;
+    }
+
+    //Methods
+    public String registrarJugador(String n){
+        Jugador aux = new Jugador(n);
+        if (!existeJugador(aux)){
+            getListaJugadores().add(aux);
+            return ("Jugador registrado con exito");
+        }
+        else
+            return ("El jugador ya está registrado actualmente");
+    }
+
+    private boolean existeJugador(Jugador j){
+        int i = 0;
+        boolean estado = false;
+        do{
+            estado = getListaJugadores().get(i).equals(j);
+            i++;
+        }
+        while((i < (getListaJugadores().size() - 1)) && (estado == false));
+        return estado;
+    }
 }
